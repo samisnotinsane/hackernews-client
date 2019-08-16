9844563a1cb1b431fa3c7e3b969279cdf430a4f4
sameen.islam <sameen.islam@accenture.com>
2019-08-16 10:47:53 +0100

    Adding method to get item by id with unit test

--- tests/test_hn.py
+++ tests/test_hn.py
@@ -12,11 +12,25 @@ class TestTopStories(unittest.TestCase):
     def setUp(self):
         self.client = NewsClient()
 
-    def test_top_stories(self):
-        top_stories = self.client.stories(fetchMax=5, type='topstories')
-        self.assertIsInstance(top_stories, list)
-        self.assertIsInstance(top_stories[0], Item)
-        self.assertIsNotNone(top_stories) 
+    def test_get_item_by_id(self):
+        item = self.client.get_item_by_id(8863)
+        self.assertIsInstance(item, Item)
+        self.assertEqual(item.by, 'dhouston')
+        self.assertEqual(item.id, 8863)
+        self.assertEqual(item.descendants, 71)
+        self.assertSetEqual(set(item.kids), set([8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876]))
+        # self.assertEqual(item.score, 111) # not making assertions as this changes over time
+        self.assertEqual(item.time, 1175714200)
+        self.assertEqual(item.title, 'My YC app: Dropbox - Throw away your USB drive')
+        self.assertEqual(item.type, 'story')
+        self.assertEqual(item.url, 'http://www.getdropbox.com/u/2/screencast.html')
+
+
+    # def test_top_stories(self):
+    #     top_stories = self.client.stories(fetchMax=5, type='topstories')
+    #     self.assertIsInstance(top_stories, list)
+    #     self.assertIsInstance(top_stories[0], Item)
+    #     self.assertIsNotNone(top_stories) 
 
     def tearDown(self):
         pass
