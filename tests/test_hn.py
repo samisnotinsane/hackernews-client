a644975090a4593569eff6087fbbb93026aa6cf7
sameen.islam <sameen.islam@accenture.com>
2019-08-16 11:17:03 +0100

    Adding unit test for get job by id

--- tests/test_hn.py
+++ tests/test_hn.py
@@ -47,12 +47,16 @@ class TestTopStories(unittest.TestCase):
         self.assertEqual(item.title, 'Ask HN: The Arc Effect')
         self.assertEqual(item.type, 'story')
         self.assertFalse(item.url)
-
-    # def test_top_stories(self):
-    #     top_stories = self.client.stories(fetchMax=5, type='topstories')
-    #     self.assertIsInstance(top_stories, list)
-    #     self.assertIsInstance(top_stories[0], Item)
-    #     self.assertIsNotNone(top_stories) 
+    
+    def test_get_job_item_by_id(self):
+        item = self.client.get_item_by_id(192327)
+        self.assertIsInstance(item, Item)
+        self.assertEqual(item.by, 'justin')
+        self.assertEqual(item.text, "Justin.tv is the biggest live video site online. We serve hundreds of thousands of video streams a day, and have supported up to 50k live concurrent viewers. Our site is growing every week, and we just added a 10 gbps line to our colo. Our unique visitors are up 900% since January.<p>There are a lot of pieces that fit together to make Justin.tv work: our video cluster, IRC server, our web app, and our monitoring and search services, to name a few. A lot of our website is dependent on Flash, and we're looking for talented Flash Engineers who know AS2 and AS3 very well who want to be leaders in the development of our Flash.<p>Responsibilities<p><pre><code>    * Contribute to product design and implementation discussions\n    * Implement projects from the idea phase to production\n    * Test and iterate code before and after production release \n</code></pre>\nQualifications<p><pre><code>    * You should know AS2, AS3, and maybe a little be of Flex.\n    * Experience building web applications.\n    * A strong desire to work on website with passionate users and ideas for how to improve it.\n    * Experience hacking video streams, python, Twisted or rails all a plus.\n</code></pre>\nWhile we're growing rapidly, Justin.tv is still a small, technology focused company, built by hackers for hackers. Seven of our ten person team are engineers or designers. We believe in rapid development, and push out new code releases every week. We're based in a beautiful office in the SOMA district of SF, one block from the caltrain station. If you want a fun job hacking on code that will touch a lot of people, JTV is for you.<p>Note: You must be physically present in SF to work for JTV. Completing the technical problem at <a href=\"http://www.justin.tv/problems/bml\" rel=\"nofollow\">http://www.justin.tv/problems/bml</a> will go a long way with us. Cheers!")
+        self.assertEqual(item.time, 1210981217)
+        self.assertEqual(item.title, "Justin.tv is looking for a Lead Flash Engineer!")
+        self.assertEqual(item.type, 'job')
+        self.assertFalse(item.url)
 
     def tearDown(self):
         pass
