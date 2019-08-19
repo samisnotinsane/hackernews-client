808e5f61723fd1597f45398155139ebd517728c2
sameen.islam <sameen.islam@accenture.com>
2019-08-19 16:37:15 +0100

    Moving main method

--- news.py
+++ news.py
@@ -14,29 +14,6 @@ def printQueryItem(news_client, item_id):
 def recentStories(news_client, n=10):
     pass
 
-def main():
-    print('hackernews-client v0.1\n\nExamples:\n\n')
-    news_client = hn.NewsClient()
-
-    print('* BEST STORIES ')
-    how_many = 3
-    print('  / showing '+ str(how_many) + ' best stories /\n')
-    printBestStories(news_client, how_many)
-
-    query = 8863 # this is of item type 'story'
-    print('* SEARCH ')
-    print('  / showing story with id: '+ str(query) + ' /\n')
-    printQueryItem(news_client, query)
-
-    query = 2921983 # this is of item type 'comment'
-    print('  / showing comment with id: '+ str(query) + ' /\n')
-    printQueryItem(news_client, query)
-
-    print('* LAST 5 STORIES \n')
-    recentStories(news_client, n=5)
-
-    print('\nExiting')
-
 def printStory(serial_no, item):
     print(
         '    ' + str(serial_no) + ' -> ' + item.title + '\n' +
@@ -66,6 +43,28 @@ def printItem(serial_no, item):
     if item.type == 'poll':
         printPoll(serial_no, item, withParts=True)
 
+def main():
+    print('hackernews-client v0.1\n\nExamples:\n\n')
+    news_client = hn.NewsClient()
+
+    print('* BEST STORIES ')
+    how_many = 3
+    print('  / showing '+ str(how_many) + ' best stories /\n')
+    printBestStories(news_client, how_many)
+
+    query = 8863 # this is of item type 'story'
+    print('* SEARCH ')
+    print('  / showing story with id: '+ str(query) + ' /\n')
+    printQueryItem(news_client, query)
+
+    query = 2921983 # this is of item type 'comment'
+    print('  / showing comment with id: '+ str(query) + ' /\n')
+    printQueryItem(news_client, query)
+
+    print('* LAST 5 STORIES \n')
+    recentStories(news_client, n=5)
+
+    print('\nExiting')
 
 if __name__ == "__main__":
     main()
