6f8761670a21c398e88af26ab3465a8bef72f46a
sameen.islam <sameen.islam@accenture.com>
2019-08-19 15:38:01 +0100

    Adding printing best stories example

--- examples.py
+++ examples.py
@@ -1,14 +1,62 @@
 from hackernews import hn
 
+def bestStories(news_client, n=10):
+    best_story_list = news_client.get_best_story(n)
+    i = 1
+    for best_story in best_story_list:
+        printItem(i, best_story)
+        i+=1
+
+def queryItem(news_client, item_id):
+    pass
+
+def recentStories(news_client, n=10):
+    pass
+
 def main():
-    print('hackernews-client v0.1\n\n')
+    print('hackernews-client v0.1\n\nExamples:\n\n')
     news_client = hn.NewsClient()
 
-    print('------------------ BEST STORIES ------------------\n')
+    print('* BEST STORIES')
+    how_many = 3
+    print('  / showing '+ str(how_many) + ' best stories /\n')
+    bestStories(news_client, how_many)
 
-    print('------------------ TOP STORIES ------------------\n')
+    query = 1164
+    print('* QUERY ITEM (Id:' + str(query) + ')\n')
+    queryItem(news_client, query)
 
-    print('------------------ NEW STORIES ------------------\n')
+    print('* LAST 5 STORIES \n')
+    recentStories(news_client, n=5)
+
+    print('\nExiting')
+
+def printStory(serial_no, item):
+    print(
+        str(serial_no) + ' -> '
+        '' + item.title + '\n' +
+        'by: ' + item.by + '\n' +
+        'url: ' + item.url + '\n'
+    )
+
+def printJob(serial_no, item):
+    pass
+
+def printComment(serial_no, item):
+    pass
+
+def printPoll(serial_no, item, withParts=False):
+    pass
+
+def printItem(serial_no, item):
+    if item.type == 'story':
+        printStory(serial_no, item)
+    if item.type == 'comment':
+        printComment(serial_no, item)
+    if item.type == 'job':
+        printJob(serial_no, item)
+    if item.type == 'poll':
+        printPoll(serial_no, item, withParts=True)
 
 
 if __name__ == "__main__":
