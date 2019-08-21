be7d742eefe0cc3f2b66d6e1171913b36301ddba
sameen.islam <sameen.islam@accenture.com>
2019-08-21 15:53:02 +0100

    Updating version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/samisnotinsane/hackernews-client'
 EMAIL = 'sameen11@hotmail.com'
 AUTHOR = 'samisnotinsane'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = 'v0.1-beta.1'
+VERSION = 'v0.1.2-beta-1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
